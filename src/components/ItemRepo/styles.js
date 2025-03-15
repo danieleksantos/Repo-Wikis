6db063ac7a041c921a00bf65864867412aa16dba
Daniele Karina dos Santos <danimistron@gmail.com>
2025-03-15 17:50:28 -0300

    sendind project before challenge

---
+++ src/components/ItemRepo/styles.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const ItemContainer = styled.div`
+    width: 80%;
+    h3 {
+        font-size: 32px;
+        color: #FAFAFA;
+    }
+    
+    p {
+        font-size: 1rem;
+        color: #FAFAFA60;
+        margin-bottom: 20px;
+
+    }
+
+    a.remover {
+    color: red;
+    }
+
+    hr {
+        color:#fafafa;
+        margin: 20px 0;
+    }
+`
